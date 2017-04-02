5f5adf4a5e2d9c8a6dea8e98b2975e9a1a8d5a59
MaciejKut <maciej.kutzmann@gmail.com>
2017-04-02 16:24:56 +0200

    looping with for and while

--- PHP-A2/exercise_7.php
+++ PHP-A2/exercise_7.php
@@ -1,1 +1,26 @@
 <?php
+
+// algorytm liczący silnie 
+// while
+
+$x = 1;
+$y = 5; // szukana liczba silni
+$silnia = 1;
+
+while ($x <= $y) {
+    $silnia = $silnia * $x;
+    $x++;
+}
+
+echo "$silnia";
+
+
+// for
+
+$silnia2 = 8;
+$sum = 1;
+
+for ($i = 1; $i <= $silnia2; $i++) {
+    $sum = $sum * $i;
+}
+echo '<br />Wynik wynosi' . $sum;
