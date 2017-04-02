fa0de0f3beaf0a48614eaf944f6b31cf251fa9e5
MaciejKut <maciej.kutzmann@gmail.com>
2017-04-02 16:09:42 +0200

    if elseif else instructions

--- PHP-A2/exercise_6.php
+++ PHP-A2/exercise_6.php
@@ -1,1 +1,27 @@
 <?php
+
+//0 - 39 pkt - ocena niedostateczna
+//* 40 - 54 pkt - ocena dopuszczająca
+//* 55 - 69 pkt - ocena dostateczna
+//* 70 - 84 pkt - ocena dobra
+//* 85 - 98 pkt - ocena bardzo dobra
+//* 99 - 100 pkt - ocena celująca
+
+
+$points = 42;
+
+if ($points >= 0 && $points <= 39) {
+    echo 'ocena niedostateczna';
+} elseif ($points > 39 && $points <= 54) {
+    echo 'ocena dopuszczająca';
+} elseif ($points > 54 && $points <= 69) {
+    echo 'ocena dostateczna';
+} elseif ($points > 69 && $points <= 84) {
+    echo 'ocena dobra';
+} elseif ($points > 84 && $points <= 98) {
+    echo 'ocena bardzo dobra';
+} elseif ($points > 98 && $points < 100) {
+    echo 'ocena celująca';
+} else {
+    echo 'Nieprawidłowa ilość punktów' . $points;
+}
