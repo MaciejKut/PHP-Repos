da7ba8ba23186a5390fde8617b387684c5618bf2
MaciejKut <maciej.kutzmann@gmail.com>
2017-04-02 13:16:32 +0200

    empty repo PHP-A2 and ex1

--- PHP-alghoritms1/1.Operatory/exercise_1.php
+++ PHP-alghoritms1/1.Operatory/exercise_1.php
@@ -1,1 +1,17 @@
 <?php
+
+$a = 3;
+$b = 4;
+$c = 5;
+
+if ($a + $b > $c && $a + $c > $b && $b + $c > $a) {
+    if ($a == $b && $b == $c) {
+        echo 'Z podanych odcinków da się zbudować trójkąt równoboczny';
+    } elseif ($a != $b && $b != $c && $a != $c) {
+        echo 'Z podanych odcinków da się zbudoać tójkąt różnoboczny';
+    } else {
+        echo 'z podanch odcinków da się zbudować trójkąt równoramienny';
+    }
+} else {
+    echo 'Z podanych odcinków nie da się zbudować tórjkąta';
+}
