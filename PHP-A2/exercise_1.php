f19b1cef1266dc9bac382915472a0e9135050772
MaciejKut <maciej.kutzmann@gmail.com>
2017-04-02 13:30:20 +0200

    Simple exercises with alghorytms

--- PHP-A2/exercise_1.php
+++ PHP-A2/exercise_1.php
@@ -1,1 +1,13 @@
 <?php
+
+$array = [2, 34, 5, 6.4, 89, 84, 5, 9, 93, 58];
+
+
+$var = $array[0];
+
+foreach ($array as $value) {
+    if ($value >= $var) {
+        $var = $value;
+    }
+}
+echo 'Największą liczbą spośród zadanej tablicy jest ' . $var;
--- PHP-A2/exercise_2.php
+++ PHP-A2/exercise_2.php
@@ -1,1 +1,11 @@
 <?php
+
+$array = [2, 34, 5, 6.4, 89, 84, 5, 9, 93, 58];
+
+$sum = 0;
+
+foreach ($array as $value) {
+    $sum = $sum + $value;
+}
+
+echo 'Suma wszystkich elemtów tablicy wynosi '.$sum;
