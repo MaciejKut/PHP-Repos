7a2ad4b777074526362123e7af969297f59c7d08
MaciejKut <maciej.kutzmann@gmail.com>
2017-04-02 14:05:34 +0200

    exercise 3 array value key

--- PHP-A2/exercise_3.php
+++ PHP-A2/exercise_3.php
@@ -1,1 +1,12 @@
 <?php
+
+$array = [2, 34, 5, 6.4, 89, 2, 84, 'two', 2, 5, 9, 93, 58, 2];
+
+$x = 2;
+
+
+foreach ($array as $key => $value) {
+    if($value === $x){
+        echo $x.'jest na pozycji'.$key.'<br />';
+    }
+}
