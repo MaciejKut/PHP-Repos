a19d9b85c50e00f14b5d33d6a21c966c0069613a
MaciejKut <maciej.kutzmann@gmail.com>
2017-04-02 15:56:42 +0200

    exercise 5 if elseif else flow

--- PHP-A2/exercise_5.php
+++ PHP-A2/exercise_5.php
@@ -1,1 +1,19 @@
 <?php
+
+$a = 2;
+$b = 8;
+$c = 4;
+
+if ($a >= $b) {
+    if ($a >= $c) {
+        echo 'Największa liczba to:'.$a;
+    } else {
+        echo 'Największa liczba to:'.$c;
+    }
+}elseif ($b >= $c) {
+    echo 'Największa liczba to:'.$b;
+} else {
+    echo 'Największa liczba to:'.$c;
+}
+
+    
