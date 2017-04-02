c41d664993d7f1a14e9bb6e714bb0767ccf457f5
MaciejKut <maciej.kutzmann@gmail.com>
2017-04-02 14:17:26 +0200

    exercise 4 triangle again

--- PHP-A2/exercise_4.php
+++ PHP-A2/exercise_4.php
@@ -1,1 +1,11 @@
 <?php
+
+$a = 5;
+$b = 3;
+$c = 7;
+
+if ($a + $b > $c && $a + $c > $b && $b + $c > $a) {
+    echo 'Z podanych odcinków' . $a . '; ' . $b . '; ' . $c . ' można zbudować trójkąt';
+} else {
+    echo 'Z podanych odcinków' . $a . '; ' . $b . '; ' . $c . ' nie można zbudować trójkąta';
+}
