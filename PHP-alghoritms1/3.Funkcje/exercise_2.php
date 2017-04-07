021bb29aa3b45ecc9bb192592e3d4e327eee0b3b
MaciejKut <maciej.kutzmann@gmail.com>
2017-04-07 19:26:46 +0200

    perfect number function

--- PHP-alghoritms1/3.Funkcje/exercise_2.php
+++ PHP-alghoritms1/3.Funkcje/exercise_2.php
@@ -1,1 +1,24 @@
 <?php
+
+function perfectNumber($num) {
+    $sumOfDivs = 0;
+
+    for ($i = 1; $i <= ($num / 2); $i++) {
+        if ($num % $i == 0) {
+            $sumOfDivs += $i;
+        }
+    }
+
+    if ($sumOfDivs == $num) {
+        echo "Liczba $num jest liczbą doskonałą bo sumofDivs = $sumOfDivs <br />";
+    } else {
+        echo "Liczba $num NIE jest liczbą doskonałą, bo suma jej dzielnikóœ wynosi $sumOfDivs <br />";
+    }
+}
+
+perfectNumber(6);
+perfectNumber(8128);
+
+perfectNumber(4);
+perfectNumber(124);
+
