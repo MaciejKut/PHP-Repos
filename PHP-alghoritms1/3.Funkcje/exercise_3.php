168fee1933a00360d0b3291d1813aace621feebb
MaciejKut <maciej.kutzmann@gmail.com>
2017-04-07 23:23:11 +0200

    change function

--- PHP-alghoritms1/3.Funkcje/exercise_3.php
+++ PHP-alghoritms1/3.Funkcje/exercise_3.php
@@ -1,1 +1,17 @@
 <?php
+
+
+function change($amount) {
+
+    $tens = floor($amount / 10);
+    $fives = floor(($amount - $tens * 10) / 5);
+    $two = floor(($amount - $tens * 10 - $fives * 5) / 2);
+    $ones = $amount - $tens * 10 - $fives * 5 - $two * 2;
+
+    echo "Value $amount cab be divided into $tens dziesiątek, $fives piątek, $two dwójek, i $ones 1złotówek <br />";
+}
+
+change(188);
+change(88);
+change(8);
+
