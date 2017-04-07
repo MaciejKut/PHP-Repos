779138be3b9d89ec0384fad27e233881ab2916d5
MaciejKut <maciej.kutzmann@gmail.com>
2017-04-07 23:36:14 +0200

    functions inside another one

--- PHP-alghoritms1/3.Funkcje/exercise_4.php
+++ PHP-alghoritms1/3.Funkcje/exercise_4.php
@@ -1,1 +1,23 @@
 <?php
+
+function max2($arg1, $arg2) {
+
+    if ($arg1 >= $arg2) {
+        return $arg1;
+    } else {
+        return $arg2;
+    }
+}
+
+function max3($arg1, $arg2, $arg3) {
+
+    $argBigger = max2($arg3, $arg2);
+
+    if ($arg1 >= $argBigger) {
+        return $arg1;
+    } else {
+        return $argBigger;
+    }
+}
+
+var_dump(max3(7, 65, 5));
