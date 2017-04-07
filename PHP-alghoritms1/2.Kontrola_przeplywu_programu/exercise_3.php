3fb52206866b088e9fb3870a9b198d90f2f7e661
MaciejKut <maciej.kutzmann@gmail.com>
2017-04-07 14:02:22 +0200

    Exercise with looping for and while

--- PHP-alghoritms1/2.Kontrola_przeplywu_programu/exercise_3.php
+++ PHP-alghoritms1/2.Kontrola_przeplywu_programu/exercise_3.php
@@ -1,1 +1,22 @@
 <?php
+
+$intNum = 75;
+$sum = 0;
+for ($i = 0; $i <= $intNum; $i++) {
+
+    $sum += $i;
+}
+
+echo 'Suma wszystkich liczb całkowitych liczby ' . $intNum . ' to: ' . $sum;
+
+echo '<br />';
+$x = 5;
+$y = 0;
+$j = 0;
+
+while ($j <= $x) {
+    $y += $j;
+    $j++;
+}
+
+echo 'Suma wszystkich liczb całkowitych liczby ' . $x . ' to: ' . $y;
