e2d8e58a321d7af2f0da8ab1c9435b69e678281b
MaciejKut <maciej.kutzmann@gmail.com>
2017-04-08 14:58:28 +0200

    referencje w funkcji

--- PHP-alghoritms1/4.Tablice/homework1.php
+++ PHP-alghoritms1/4.Tablice/homework1.php
@@ -1,1 +1,26 @@
 <?php
+
+function inArray($foo, array $array, &$n) {
+
+    foreach ($array as $key => $value) {
+
+        if ($n == $value) {
+            $n = $key;
+        }
+    }
+
+    foreach ($array as $value) {
+        if ($foo === $value) {
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+
+$array = [3, 54, 59, 005, 4, 13, 14];
+
+$g = 59;
+
+var_dump(inArray(4, $array, $g));
+
+var_dump($g);
