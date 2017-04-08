0dfc6a98810be5c28e8dd0233f3b1ee5809aed30
MaciejKut <maciej.kutzmann@gmail.com>
2017-04-08 13:00:44 +0200

    the greatest in array

--- PHP-alghoritms1/4.Tablice/exercise_3.php
+++ PHP-alghoritms1/4.Tablice/exercise_3.php
@@ -1,1 +1,30 @@
 <?php
+
+$array = [3, 54, 59, 005, 4, 13, 14];
+
+function theGreatest(array $array) {
+
+    $theGreatest = '';
+
+    foreach ($array as $value) {
+
+        if ($theGreatest <= $value) {
+            $theGreatest = $value;
+        }
+    }
+    echo "największa liczba w danej tablicy to $theGreatest";
+}
+
+theGreatest($array);
+
+function biggestInArray($array) {
+    $bigOne = 0;
+    for ($i = 0; $i < count($array); $i++) {
+        if ($bigOne <= $array[$i]) {
+            $bigOne = $array[$i];
+        }
+    }
+    echo "Największe jest $bigOne";
+}
+
+biggestInArray($array);
