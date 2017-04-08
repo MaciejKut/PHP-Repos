6faf147e725022f7ce6bd1f4fd0b0aa195f1be9d
MaciejKut <maciej.kutzmann@gmail.com>
2017-04-08 13:17:28 +0200

    less than average in array

--- PHP-alghoritms1/4.Tablice/exercise_4.php
+++ PHP-alghoritms1/4.Tablice/exercise_4.php
@@ -1,1 +1,22 @@
 <?php
+
+$array = [4, 3, 5, 3, 5, 6, 3, 2, 4, 5];
+
+function lessThanAvrg(array $array) {
+    $sum = '';
+
+    foreach ($array as $value) {
+        $sum += $value;
+    }
+
+    $avr = $sum / count($array);
+
+    echo 'Mniejsze od średniej są: ';
+    foreach ($array as $val) {
+        if ($val < $avr) {
+            echo "$val , ";
+        }
+    }
+}
+
+lessThanAvrg($array);
