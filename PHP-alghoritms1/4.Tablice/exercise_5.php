097b857ee3aec237f3d9f9565543c2290f2ee4d0
MaciejKut <maciej.kutzmann@gmail.com>
2017-04-08 14:06:25 +0200

    function that checks if the value is in array given in second argument

--- PHP-alghoritms1/4.Tablice/exercise_5.php
+++ PHP-alghoritms1/4.Tablice/exercise_5.php
@@ -1,1 +1,15 @@
 <?php
+
+function inArray($foo, array $array) {
+
+    foreach ($array as $value) {
+        if ($foo === $value) {
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+
+$array = [3, 54, 59, 005, 4, 13, 14];
+
+var_dump(inArray(4, $array));
