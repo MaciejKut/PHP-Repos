05745ac5e7c5bd1bd03de9a73a3f7302477a0d37
MaciejKut <maciej.kutzmann@gmail.com>
2017-04-08 16:16:28 +0200

    bubble sort alghoritm

--- PHP-alghoritms1/4.Tablice/homework2.php
+++ PHP-alghoritms1/4.Tablice/homework2.php
@@ -1,1 +1,25 @@
 <?php
+
+$array = [4, 2, 5, 4, 5, 6, 2, -1, 0, 45, 3, 8];
+
+function bubbleSort($array) {
+
+    $l = count($array);
+
+    for ($i = 0; $i < $l; $i++) {
+
+        for ($j = 0; $j < $l; $j++) {
+
+            if ($array[$i] < $array[$j]) {
+                $tmp = $array[$i];
+                $array[$i] = $array[$j];
+                $array[$j] = $tmp;
+            }
+        }
+    }
+    return $array;
+}
+
+var_dump(bubbleSort($array));
+
+
