c1d736a3a872d5c3dd819d710d236e633131737e
MaciejKut <maciej.kutzmann@gmail.com>
2017-04-08 12:40:44 +0200

    2 dimensional arrays operations

--- PHP-alghoritms1/4.Tablice/exercise_2.php
+++ PHP-alghoritms1/4.Tablice/exercise_2.php
@@ -1,1 +1,35 @@
 <?php
+
+$array1 = array(
+    [1, 2, 3, 5],
+    [4, 5, 6, 5],
+    [7, 8, 9, 9],
+    [7, 8, 9, 9]
+);
+
+function print2DTable($table) {
+    foreach ($table as $value) {
+        foreach ($value as $val) {
+            echo $val;
+            echo ' ';
+        }
+        echo '<br />';
+    }
+}
+
+print2DTable($array1);
+
+function getMatrixTrace($array) {
+    $trace = 0;
+
+    foreach ($array as $key1 => $i) {
+        foreach ($i as $key2 => $value) {
+            if ($key1 == $key2) {
+                $trace += $value;
+            }
+        }
+    }
+    echo $trace;
+}
+
+getMatrixTrace($array1);
