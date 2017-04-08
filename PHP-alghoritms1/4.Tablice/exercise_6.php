a4be9f9c45d850d0bf1d6622d8dc51d680bceed1
MaciejKut <maciej.kutzmann@gmail.com>
2017-04-08 14:37:08 +0200

    function that create multiArray

--- PHP-alghoritms1/4.Tablice/exercise_6.php
+++ PHP-alghoritms1/4.Tablice/exercise_6.php
@@ -1,1 +1,17 @@
 <?php
+
+function createMultiTable($n) {
+
+    $arrayIn = [];
+    $arrayOut = [];
+
+    for ($i = 1; $i <= $n; $i++) {
+        for ($j = 1; $j <= $n; $j++) {
+            $arrayIn[] = $j;
+        }
+        $arrayOut[] = $arrayIn;
+        unset($arrayIn);
+    } return $arrayOut;
+}
+
+var_dump(createMultiTable(9));
