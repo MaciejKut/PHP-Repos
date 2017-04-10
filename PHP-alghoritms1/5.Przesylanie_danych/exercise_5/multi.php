cf79201b9119d3443b35bca28143af0db6310ccf
MaciejKut <maciej.kutzmann@gmail.com>
2017-04-10 02:28:39 +0200

    ex6 index and multi

--- PHP-alghoritms1/5.Przesylanie_danych/exercise_5/multi.php
+++ PHP-alghoritms1/5.Przesylanie_danych/exercise_5/multi.php
@@ -18,19 +18,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     </head>
     <body>
 
-<?php
-echo '<table style="border: 1px dashed black;">';
-for ($i = 1; $i <= $height; $i++) {
-    echo '<tr style="border: 1px dashed black;">';
-    for ($j = 1; $j <= $width; $j++) {
-        echo '<td style="border: 1px dashed black;">';
-        echo "$i * $j =" . $i * $j;
-        echo '</td>';
-    }
-    echo '<tr/>';
-}
-echo '<table>';
-?>
+        <?php
+        echo '<table style="border: 1px dashed black;">';
+        for ($i = 1; $i <= $height; $i++) {
+            echo '<tr style="border: 1px dashed black;">';
+            for ($j = 1; $j <= $width; $j++) {
+                echo '<td style="border: 1px dashed black;">';
+                echo "$i * $j =" . $i * $j;
+                echo '</td>';
+            }
+            echo '<tr/>';
+        }
+        echo '<table>';
+        ?>
 
 
     </body>
--- PHP-alghoritms1/5.Przesylanie_danych/exercise_6/index.php
+++ PHP-alghoritms1/5.Przesylanie_danych/exercise_6/index.php
@@ -1,35 +1,54 @@
 <?php
-
-// tutaj umieść kod który będzie cenzurował napis
-
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['userText'])) {
+        $userText = $_POST['userText'];
+    }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Ćwiczenie 4</title>
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Ćwiczenie 4</title>
+    </head>
+    <body>
 
-<?php
+        <?php
+        if (!isset($_POST['userAgreement'])) {
+            echo $userText;
+        } else {
+            $arrayText = explode(' ', $userText);
 
-// tutaj umieść kod który będzie wyświetlał napis
+            $modificatedText = [];
+            foreach ($arrayText as $value) {
+                if ($value === 'kurwa') {
+                    $tmp = strlen($value);
+                    $word = '';
+                    for ($i = 1; $i <= $tmp; $i++) {
+                        $word .= '*';
+                    }
+                    $modificatedText[] = $word;
+                } else {
+                    $modificatedText[] = $value;
+                }
+            }
+        }
+        var_dump($modificatedText);
+        ?>
 
-?>
 
+        <form action="#" method="POST">
+            <label>
+                Twój tekst:
+                <input type="text" name="userText">
+            </label>
+            <label>
+                Jestem świadomy konsekwencji
+                <input type="checkbox" name="userAgreement">
+            </label>
+            <input type="submit">
+        </form>
 
-<form action="#" method="POST">
-    <label>
-        Twój tekst:
-        <input type="text" name="userText">
-    </label>
-    <label>
-        Jestem świadomy konsekwencji
-        <input type="checkbox" name="userAgreement">
-    </label>
-    <input type="submit">
-</form>
-
-</body>
+    </body>
 </html>
