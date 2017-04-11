98760a2f55f2d116787c70f1ae12abd65287d049
MaciejKut <maciej.kutzmann@gmail.com>
2017-04-12 01:20:35 +0200

    Lotto game vers. 01

---
+++ Advanced-aghoritms/ProjectsinPHP/lottoGame.php
@@ -0,0 +1,103 @@
+<?php
+$start = 1;
+$stop = 7;
+?>
+<html>
+    <head>
+        <style>
+            h1{
+                display: inline-block;
+
+            }
+            body {
+                /* Background pattern from Subtle Patterns */
+                background-image: url('img/symphony.png');
+            }
+        </style>
+    </head>
+    <div id="container" style="width: 1000px; margin-left: auto; margin-right:auto;">
+
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (isset($_POST['choosed']) && (count($_POST['choosed'])) === 6) {
+                $picked = $_POST['choosed'];
+                sort($picked);
+                echo 'Wybrałeś: ';
+                foreach ($picked as $value) {
+                    echo $value . ', ';
+                }
+            } else {
+                echo '<em style="font-size: 18px;">Nie prawidłowe dane zaznacz dokładnie 6 liczb</em>';
+            }
+            // losowanie liczb
+            echo '<br /> Wylosowane liczby to: ';
+
+            $range = range($start, $stop);
+            shuffle($range);
+
+            $wylosowane = array_slice($range, 0, 6);
+            sort($wylosowane);
+            foreach ($wylosowane as $value) {
+                echo $value . ', ';
+            }
+
+            if (isset($picked)) {
+                $matching = array_intersect($picked, $wylosowane);
+                if (count($matching) > 0) {
+                    switch (count($matching)) {
+                        case 1:
+                        case 2:
+
+                            echo '<br/>Nieźle graj dalej!<br/>';
+                            foreach ($matching as $value) {
+                                echo '<h1>' . $value . ',</h1> ';
+                            }
+                            break;
+                        case 3:
+                            echo '<br/>Wygrywasz 50zł<br/>';
+                            foreach ($matching as $value) {
+                                echo '<h1>' . $value . ',</h1> ';
+                            }
+                            break;
+                        case 4:
+                            echo '<br/>Wygrywasz 5000zł<br/>';
+                            foreach ($matching as $value) {
+                                echo '<h1>' . $value . ',</h1> ';
+                            }
+                            break;
+                        case 5:
+                            echo '<br/>Wygrywasz 50000zł<br/>';
+                            foreach ($matching as $value) {
+                                echo '<h1>' . $value . ',</h1> ';
+                            }
+                            break;
+                        case 6:
+                            echo '<br/>Wygrywasz 7 000 000 zł<br/>';
+                            foreach ($matching as $value) {
+                                echo '<h1>' . $value . ',</h1> ';
+                            }
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+
+
+        echo '<div style="margin-left: auto; margin-right:auto; width: 400px;">'
+        . 'Proszę wylosować 6 liczb.<form method="POST" action="#">';
+        echo '<fieldset>';
+        for ($i = $start; $i <= $stop; $i++) {
+
+            echo '<label>' . $i . '<input type="checkbox" name="choosed[]" value="' . $i . '"/></label>';
+        }
+        echo '<input type="submit" style="display: block; margin-top: 20px"/></fieldset>';
+        echo '<form></div>';
+        ?>
+
+        <div></div>
+    </div>
+</html>
