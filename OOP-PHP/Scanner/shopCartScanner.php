32c507e250288cf6e59608ae45d56ff7ab948f6e
MaciejKut <maciej.kutzmann@gmail.com>
2017-04-19 22:41:29 +0200

    repos with exercises MySQL

--- OOP-PHP/Scanner/shopCartScanner.php
+++ OOP-PHP/Scanner/shopCartScanner.php
@@ -6,19 +6,18 @@ $x = new Product('cebula', 1, 3, 'zwykła obrana');
 $x2 = new Product('ziemniaki', 4, 25, 'Ziemniaki kopane');
 $cart1 = new Scanner();
 
-var_dump($cart1);
+//var_dump($cart1);
 
 $cart1->addProduct($x);
 $cart1->addProduct($x2);
 
 
-var_dump($cart1);
-
+//var_dump($cart1);
 //$cart1->deleteProduct('cebula');
 $cart1->changeQty('cebula', 8);
 
 $cart1->printRecipt();
 
-var_dump($cart1);
+//var_dump($cart1);
 
 
