e74168504920c866a0a456762c5ff7c5dd830829
MaciejKut <maciej.kutzmann@gmail.com>
2017-04-20 10:32:21 +0200

    creating connection and database

--- MySQL/1.Dzien_1/1Exercises/exercise1.php
+++ MySQL/1.Dzien_1/1Exercises/exercise1.php
@@ -1,2 +1,11 @@
-<?php 
+<?php
 
+$conn = new mysqli('localhost', 'root', 'maciej');
+
+
+if ($conn->connect_error) {
+    die("Polaczenie nieudane. Blad: " . $conn->connect_error);
+}
+echo("Polaczenie udane.");
+
+$conn->query('CREATE DATABASE Maciej2');
