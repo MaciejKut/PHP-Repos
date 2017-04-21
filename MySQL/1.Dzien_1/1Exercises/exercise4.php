528e21e955f6bdd778204ba5d899966eedb69d90
MaciejKut <maciej.kutzmann@gmail.com>
2017-04-21 14:18:06 +0200

    sql Select,  fetch_assoc  fetch_row num_rows

---
+++ MySQL/1.Dzien_1/1Exercises/exercise4.php
@@ -0,0 +1,22 @@
+<?php
+
+$conn = new mysqli('localhost', 'root', 'maciej', 'Maciej2');
+
+if ($conn->connect_error) {
+    die("Error occured:" . $conn->error);
+}
+
+$sql = "SELECT * FROM Product";
+
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+
+    foreach ($result as $value) {
+        echo "name = ".$value['name']." ";
+        echo "description = ".$value['description']." ";
+        echo "id = ".$value['id']." </br>";
+    }
+}
+
+$conn->close();
+$conn = null;
